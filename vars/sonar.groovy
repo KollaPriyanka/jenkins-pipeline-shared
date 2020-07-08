01d3a9ab71e59a2d8d51821a8f1df1c8c176d427
KollaPriyanka <55626247+KollaPriyanka@users.noreply.github.com>
2020-07-08 19:05:16 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,10 +1,18 @@
 def call(String msg = 'SONAR'){
   echo "${msg}"
  
-  environment{
+  /*environment{
     scannerHome=tool 'sonarscanner'  
   }
   withSonarQubeEnv('sonarqube'){
                      sh 'mvn sonar:sonar'
+                }*/
+  environment{
+                 sonarscanner = tool 'sonarscanner'
+                 }
+            
+               withSonarQubeEnv('sonarqube'){
+                     sh '${sonarscanner}/bin/sonar-scanner -Dproject.settings=./sonar-project.properties'
                 }
+            
 }
